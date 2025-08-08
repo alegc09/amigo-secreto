9ed707addde6d43bf00a83cce701dd7f45be04af
Alejandro <alejandrogtzcruz09@gmail.com>
2025-08-07 23:47:29 -0600

    Se agregan las funciones agregarAmigo(), limpiarCaja(), listarAmigos(), limpiarLista() y sortearAmigo() al app.js

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let amigoInput= document.getElementById('amigo').value;
+
+    if(amigoInput == ''){
+        alert('Por favor escribe un nombre valido');
+        return;
+    }
+    amigos.push(amigoInput);
+    limpiarCaja();
+    listarAmigos(amigoInput);
+}
+
+function limpiarCaja(){
+    let valorCaja = document.querySelector('#amigo');
+    valorCaja.value = '';
+}
+
+function listarAmigos(elemento){
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML += `<li>${elemento}</li>`;
+}
+
+function limpiarLista(){
+      document.getElementById('listaAmigos').innerHTML = '';
+}
+
+function sortearAmigo(){
+    if(amigos.length > 0){
+        limpiarLista();
+        let tamañoArray = amigos.length;
+        let resultado = Math.floor(Math.random()*tamañoArray);
+        document.getElementById('resultado').innerHTML = `<li>El amigo secreto es: ${amigos[resultado]}</li>`;
+    }
+    else{
+        alert('Primero tiene que agregar amigos');
+        return;
+    }
+    
+}
